683339bb4f2b676e8d0946dec1d78e4e16b69e92
honggildong <dangtang@paran.com>
2023-09-11 01:15:37 +0900

    20230911

--- 사칙/src/5622.py
+++ 사칙/src/5622.py
@@ -7,10 +7,8 @@ for i in S:
     cnt=0
     for j in ar:   
         num = j.find(i)
-        # print(num)
         if num>=0:
             second+=(cnt+3)
-            # print(second)
         else:
             cnt+=1
-print(second)
+
