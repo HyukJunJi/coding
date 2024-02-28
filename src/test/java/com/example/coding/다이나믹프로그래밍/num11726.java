37c7c9174ce65b3cc8524671df9ce96d43657df6
HyukJunJi <whrnjsrkdls@gmail.com>
2024-02-29 00:16:45 +0900

    BOJ: DP

---
+++ src/test/java/com/example/coding/다이나믹프로그래밍/num11726.java
@@ -0,0 +1,22 @@
+package com.example.coding.다이나믹프로그래밍;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class num11726 {
+  public static long[]dp;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    dp = new long[1001];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3 ; i < n+1; i++){
+      dp[i] = (dp[i-1]+dp[i-2])%10007;
+    }
+    System.out.println(dp[n]);
+  }
+}
